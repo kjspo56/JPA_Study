8bb8de6fd67af0ceb8dd0782b8b17f68ff89c355
kjspo56 <kjspo56@gmail.com>
2022-02-15 23:11:12 +0900

    Mac Test

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
         //emf = entitymanagerfactory
 
         EntityManager em = emf.createEntityManager();
